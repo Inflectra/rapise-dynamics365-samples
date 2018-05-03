2ae9d850025a6b329c9f984feaa407390617387c
dmarkovtsev <maverick-@users.noreply.github.com>
2018-05-03 13:28:39 +0300

    Recording process is published on YouTube

---
+++ D365OpsPurchaseOrderRecorded/D365OpsPurchaseOrderRecorded.js
@@ -0,0 +1,12 @@
+
+
+
+function Test(params)
+{
+	Navigator.Open();
+	RVL.DoPlayScript();
+}
+
+g_load_libraries=["%g_browserLibrary:Chrome HTML%", "DomDynamicsAX"];
+
+
---
+++ D365OpsPurchaseOrderRecorded/D365OpsPurchaseOrderRecorded.objects.js
@@ -0,0 +1,394 @@
+var saved_script_objects={
+	DAT:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Button",
+	"object_name": "DAT",
+	"object_library": "Chrome HTML",
+	"window_name": "Dashboard -- Operations",
+	"xpath": "//button[@id='Company']",
+	"title": "Dashboard -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=DAT&mi=DefaultDashboard"
+},
+	SysCompanyChooser_DataArea_id:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXComboBox",
+	"object_flavor": "ComboBox",
+	"object_name": "SysCompanyChooser_DataArea_id",
+	"window_name": "Dashboard -- Operations",
+	"xpath": "//div[contains(@id,'SysCompanyChooser') and contains(@id,'DataArea_id')]",
+	"title": "Dashboard -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=DAT&mi=DefaultDashboard"
+},
+	SysCompanyChooserLookup_Grid:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "SysCompanyChooserLookup_Grid",
+	"window_name": "Dashboard -- Operations",
+	"xpath": "//div[contains(@id,'SysCompanyChooserLookup') and contains(@id,'Grid') and @role='grid']",
+	"title": "Dashboard -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=DAT&mi=DefaultDashboard"
+},
+	NavigationSearchBox:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXNavigationSearch",
+	"object_flavor": "Search",
+	"object_name": "NavigationSearchBox",
+	"window_name": "Dashboard -- Operations",
+	"xpath": "//div[@id='NavigationSearch']",
+	"title": "Dashboard -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=defaultdashboard"
+},
+	_New:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Button",
+	"object_name": "New",
+	"object_library": "Chrome HTML",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//button[contains(@id,'purchtablelistpageopen') and contains(@id,'SystemDefinedNewButton')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	BODY:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Generic",
+	"object_name": "BODY",
+	"object_library": "Chrome HTML",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//body",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	PurchCreateOrder_PurchTable_Orde:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXComboBox",
+	"object_flavor": "ComboBox",
+	"object_name": "PurchCreateOrder_PurchTable_OrderAccount",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'PurchCreateOrder') and contains(@id,'PurchTable_OrderAccount')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	VendTable_SysTL_Grid:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "VendTable_SysTL_Grid",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'VendTable_SysTL') and contains(@id,'Grid') and @role='grid']",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	PurchCreateOrder_PurchTable_Inve:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXComboBox",
+	"object_flavor": "ComboBox",
+	"object_name": "PurchCreateOrder_PurchTable_InventLocationId",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'PurchCreateOrder') and contains(@id,'PurchTable_InventLocationId')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	InventLocationIdLookup_GridInven:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "InventLocationIdLookup_GridInventLocation",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'InventLocationIdLookup') and contains(@id,'GridInventLocation') and @role='grid']",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	OK:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Button",
+	"object_name": "OK",
+	"object_library": "Chrome HTML",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//button[contains(@id,'PurchCreateOrder') and contains(@id,'OK')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+//========== Recorded at Friday, April 27, 2018 6:13:58 AM: ======
+
+	purchtablelistpageopen_LineSpec:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "purchtablelistpageopen_LineSpec",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'purchtablelistpageopen') and contains(@id,'LineSpec') and @role='grid']",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	InventItemIdLookupPurchase_Grid:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "InventItemIdLookupPurchase_Grid",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'InventItemIdLookupPurchase') and contains(@id,'Grid') and @role='grid']",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	UnitOfMeasureLookup_GridConverti:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "DomDynamicsAXGrid",
+	"object_flavor": "Grid",
+	"object_name": "UnitOfMeasureLookup_GridConvertible",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//div[contains(@id,'UnitOfMeasureLookup') and contains(@id,'GridConvertible') and @role='grid']",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	Add_line:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Button",
+	"object_name": "Add line",
+	"object_library": "Chrome HTML",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//button[contains(@id,'purchtablelistpageopen') and contains(@id,'LineStripNew')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+},
+	Save:{
+	"locations": [
+		{
+			"locator_name": "HTML",
+			"location": {
+				"xpath": "param:xpath",
+				"url": "param:url",
+				"title": "param:title"
+			}
+		}
+	],
+	"window_class": "Chrome_WidgetWin_1",
+	"object_text": "Chrome Legacy Window",
+	"object_role": "ROLE_SYSTEM_WINDOW",
+	"object_class": "Chrome_RenderWidgetHostHWND",
+	"version": 0,
+	"object_type": "HTMLObject",
+	"object_flavor": "Button",
+	"object_name": "Save",
+	"object_library": "Chrome HTML",
+	"window_name": "Purchase orders -- Operations",
+	"xpath": "//button[contains(@id,'purchtablelistpageopen') and contains(@id,'SystemDefinedSaveButton')]",
+	"title": "Purchase orders -- Operations",
+	"url": "https://d365oef0aab7797234d85aos.cloudax.dynamics.com/?cmp=uspi&mi=PurchTableListPageOpen"
+}
+};
